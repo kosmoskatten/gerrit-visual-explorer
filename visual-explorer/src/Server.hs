fa70d6cc07d2332135566e85882014c178cb33d8
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-09-28 16:41:40 +0200

    Some more server stuff.

--- visual-explorer/src/Server.hs
+++ visual-explorer/src/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
     ( main
     ) where
@@ -6,6 +7,9 @@ import Data.ByteString (ByteString)
 import Gerrit.Store.Acid (openLocalState)
 import Gerrit.Store.Types (emptyStore)
 import Logger (createLogger, logMsg)
+import Network.HTTP.Types
+import Network.Wai.Conduit
+import Network.Wai.Handler.Warp (run)
 import State (State (..))
 import System.Environment (getArgs, lookupEnv)
 import Text.Printf (printf)
@@ -28,7 +32,33 @@ main = do
 
 runServer :: Int -> State -> IO ()
 runServer port state = do
-    logMsg (logger state) $ printf "Starting server on port: %d" port 
+    logMsg (logger state) $ printf "Starting server on port: %d" port
+    run port $ application state
 
-    
+application :: State -> Application
+application state request respond = respond =<< route state request
+
+route :: State -> Request -> IO Response
+route state request = do
+    case rawPathInfo request of
+        "/commits-per-active-day"
+           | requestMethod request == "GET" -> 
+               handleCommitsPerActiveDay state
+           | otherwise                      -> 
+               handleNotAllowed ["GET"]
+        _                                  -> 
+            return handleNotFound
+
+handleCommitsPerActiveDay :: State -> IO Response
+handleCommitsPerActiveDay = undefined
+
+handleNotAllowed :: [ByteString] -> IO Response
+handleNotAllowed allow =
+    return $ responseLBS status405 
+                         [("Allow", "," `BS.intercalate` allow)] ""
+
+handleNotFound :: Response
+handleNotFound = responseLBS status404
+                             [("Content-Type", "text/plain")]
+                             "Resource not found"
     
